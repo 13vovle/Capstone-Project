7e03e804d36a023a1ffbf79b7eca0dbc497a7369
Aneri Dalal <anku127@gmail.com>
2021-04-24 17:27:20 -0400

    admin logout

--- Frontend/project/src/app/app-routing.module.ts
+++ Frontend/project/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdministratorComponent } from './administrator/administrator.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   {path: "", component:SignInComponent},
-  {path: "\sign-up", component:SignUpComponent}
+  {path: "\sign-up", component:SignUpComponent},
+  {path: "\admin", component:AdministratorComponent}
 ];
 
 @NgModule({
