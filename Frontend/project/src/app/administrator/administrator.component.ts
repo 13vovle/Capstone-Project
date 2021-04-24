75ef8abca3b340e70c4c34ce7792f394fa12d681
Aneri Dalal <anku127@gmail.com>
2021-04-24 18:01:52 -0400

    admin add employee works

--- Frontend/project/src/app/administrator/administrator.component.ts
+++ Frontend/project/src/app/administrator/administrator.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-administrator',
@@ -8,12 +9,18 @@ import { Router } from '@angular/router';
 })
 export class AdministratorComponent implements OnInit {
 
-  constructor(public router: Router) { }
+  constructor(public router: Router, public empSer: EmployeeService) { }
 
   ngOnInit(): void { }
 
   logout() {
     sessionStorage.removeItem("admin");
     this.router.navigate([""]);
+  }
+
+  addEmployee(empRef: any) {
+    empRef.pass = "NewPass123";
+    console.log(empRef);
+    this.empSer.addEmployee(empRef);
   }
 }
---
+++ Frontend/project/src/app/employee.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor(public http: HttpClient) { }
+
+  addEmployee(newEmp: any) {
+    this.http.post("http://localhost:9090/emp/addEmployee", newEmp, { responseType: "text" }).
+      subscribe(result => console.log(result), error => console.log(error));
+  }
+}
