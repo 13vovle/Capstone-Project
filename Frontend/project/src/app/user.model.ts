b199b820567ec38f7d961d49d963682dcfa8f7a9
Aneri Dalal <anku127@gmail.com>
2021-04-28 12:50:45 -0400

    added default account number and amount to users

--- Frontend/project/src/app/user.model.ts
+++ Frontend/project/src/app/user.model.ts
@@ -3,13 +3,14 @@ export class User{
                 public firstName:string,
                 public lastName:string,
                 public birthday:Date,
-                public address:string,
+                public address:JSON,
                 public email:string,
                 public hashedPassword:string,
                 public funds:string,
                 public cart:any,    // once Product file is created it will be Array<Product>
                 public orders:any,  // one Order file is created it will be Array<Order>
                 public isLockedOut:boolean,
-                public numberOfTries:number
+                public numberOfTries:number,
+                public account:JSON
                 ){}
 }
